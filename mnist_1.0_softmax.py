8a739c829169471e721460d41d394ab1a5cdf96b
Marzuk Rashid <marzukr@gmail.com>
2017-12-13 22:05:31 -0600

    multi-layer neural net

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -44,16 +44,29 @@ X = tf.placeholder(tf.float32, [None, 28, 28, 1])
 # correct answers will go here
 Y_ = tf.placeholder(tf.float32, [None, 10])
 # weights W[784, 10]   784=28*28
-W = tf.Variable(tf.zeros([784, 10]))
+W1 = tf.Variable(tf.truncated_normal([784, 200], stddev=0.1))
 # biases b[10]
-b = tf.Variable(tf.zeros([10]))
+B1 = tf.Variable(tf.zeros([200]))
+
+W2 = tf.Variable(tf.truncated_normal([200, 100], stddev=0.1))
+B2 = tf.Variable(tf.zeros([100]))
+W3 = tf.Variable(tf.truncated_normal([100, 60], stddev=0.1))
+B3 = tf.Variable(tf.zeros([60]))
+W4 = tf.Variable(tf.truncated_normal([60, 30], stddev=0.1))
+B4 = tf.Variable(tf.zeros([30]))
+W5 = tf.Variable(tf.truncated_normal([30, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
 
 # flatten the images into a single line of pixels
 # -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
-XX = tf.reshape(X, [-1, 784])
+XX = tf.reshape(X, [-1, 28*28])
 
 # The model
-Y = tf.nn.softmax(tf.matmul(XX, W) + b)
+Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
+Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+Y = tf.nn.softmax(tf.matmul(Y4, W5) + B5)
 
 # loss function: cross-entropy = - sum( Y_i * log(Yi) )
 #                           Y: the computed output vector
@@ -71,11 +84,11 @@ correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # training, learning rate = 0.005
-train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(0.005).minimize(cross_entropy) # Possible to use GradientDecentOptimizer, converge slower, less acurate
 
 # matplotlib visualisation
-allweights = tf.reshape(W, [-1])
-allbiases = tf.reshape(b, [-1])
+allweights = tf.concat([tf.reshape(W1, [-1]), tf.reshape(W2, [-1]), tf.reshape(W3, [-1]), tf.reshape(W4, [-1]), tf.reshape(W5, [-1])], 0)
+allbiases  = tf.concat([tf.reshape(B1, [-1]), tf.reshape(B2, [-1]), tf.reshape(B3, [-1]), tf.reshape(B4, [-1]), tf.reshape(B5, [-1])], 0)
 I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)  # assembles 10x10 images by default
 It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)  # 1000 images on 25 lines
 datavis = tensorflowvisu.MnistDataVis()
@@ -111,7 +124,7 @@ def training_step(i, update_test_data, update_train_data):
     sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
 
 
-datavis.animate(training_step, iterations=2000+1, train_data_update_freq=10, test_data_update_freq=50, more_tests_at_start=True)
+datavis.animate(training_step, iterations=10000+1, train_data_update_freq=20, test_data_update_freq=100, more_tests_at_start=True)
 
 # to save the animation as a movie, add save_movie=True as an argument to datavis.animate
 # to disable the visualisation use the following line instead of the datavis.animate line
